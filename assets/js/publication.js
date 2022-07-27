4e7808ec2d319b0c55b032929f93e97a714db2de
SoongE <oh1.seungmin@gmail.com>
2022-07-27 16:49:53 +0900

    Modify publication detail header to overlap default header

---
+++ assets/js/publication.js
@@ -0,0 +1,13 @@
+header = document.querySelector("body > div.container > div.row > div > h1 > a");
+header.style.color = "white";
+
+header_menu1 = document.querySelector("body > div.container > div.row > div > nav > div > ul > li:nth-child(1) > a");
+header_menu1.style.color = "white";
+header_menu2 = document.querySelector("body > div.container > div.row > div > nav > div > ul > li:nth-child(2) > a");
+header_menu2.style.color = "white";
+header_menu3 = document.querySelector("body > div.container > div.row > div > nav > div > ul > li:nth-child(3) > a");
+header_menu3.style.color = "white";
+header_menu4 = document.querySelector("body > div.container > div.row > div > nav > div > ul > li:nth-child(4) > a");
+header_menu4.style.color = "white";
+header_menu5 = document.querySelector("body > div.container > div.row > div > nav > div > ul > li:nth-child(5) > a");
+header_menu5.style.color = "white";
