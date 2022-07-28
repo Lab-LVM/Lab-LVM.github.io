ed60dcec4fab9c35c56fa0d9058e459f26c5e52e
hankyul2 <consistant1y@ajou.ac.kr>
2022-07-28 14:41:48 +0900

    fix: color mismatch in publications
    - add hover class
    - change hover class in js

--- assets/js/publication.js
+++ assets/js/publication.js
@@ -4,6 +4,9 @@ header.style.color = "white";
 for (var i = 1; i < 6; i++) {
     header_menu = document.querySelector("body > div.container > div.row > div > nav > div > ul > li:nth-child("+i+") > a");
     header_menu.style.color = "white";
+    nav_link = document.querySelector("body > div.container > div.nav-box > li:nth-child("+i+")");
+    nav_link.classList.remove("white-hover", "gray-hover");
+    nav_link.classList.add('gray-hover');
 }
 
 for (var i = 1; i < 6; i++) {
@@ -14,5 +17,5 @@ for (var i = 1; i < 6; i++) {
 item = document.querySelector("body > div.container > div.nav-box");
 item.style.borderColor = "white";
 
-// item = document.querySelector("body > div.container > div.nav-box > li:nth-child(2)");
-// item.style.backgroundColor = "black";
+nav_bar = document.getElementsByClassName("main-nav-bar")[0];
+nav_bar.style.filter = "invert(100%) sepia(17%) saturate(2%) hue-rotate(234deg) brightness(111%) contrast(101%)";
